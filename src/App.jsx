832ef8f6f6674d34ca9af10a76f2e9727b77313c
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2024-01-08 19:18:09 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 
-import { NameComponent, ObjName,ArrData,ObjListData,ComplexData,LinksData } from "./components/questions.jsx";
+import { NameComponent, ObjName, ArrData, ObjListData, ComplexData, LinksData } from "./components/questions.jsx";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="main">
+    <div className="main container">
       <div>
+      <h1 className="question">Question 1</h1>
         <NameComponent />
         <ObjName />
         <ArrData />
         <ObjListData />
         <ComplexData />
+        <hr />
+        <div>
+          <h1 className="question">Question 2</h1>
+          <h3>1. Import Something from 'package'</h3>
+          <p>This is like unwrapping a toy and saying, "I want a surprise toy!" You don't know exactly which toy you'll get, but you're excited to see what's inside. So, when you say <b>import Something from 'package'</b>, you're getting a surprise toy from the package. You can play with it without knowing its specific name.</p>
+
+          <h3>{"2. Import {Something} from 'package'"}</h3>
+          <p>{`Now, this is a bit different. It's like looking at the wrapping paper and saying, "I want the toy with the robot
+   picture." You're being specific about which toy you want. So, when you say import { Something } from 'package', you're
+    choosing a toy by its name, like the one with the robot picture on the wrapper. Then you can play with that specific 
+    toy.`}</p>
+          <hr />
+        </div>
+        <div></div>
+        <h1 className="question">Question 3</h1>
+        <LinksData />
       </div>
     </div>
   );
--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -1,4 +1,7 @@
 import "./components.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 const NameComponent = () => {
   const name = "Muhammad Ahmed";
@@ -58,13 +61,13 @@ const ComplexData = () => {
           <div key={i}>
             <h3>{v.company}</h3>
             <ul className="data-ul">
-               {v.jobs.map((v,i)=>{
-                return(
-                    <li key={i}>{v}</li>
+              {v.jobs.map((v, i) => {
+                return (
+                  <li key={i}>{v}</li>
                 )
-               })} 
+              })}
             </ul>
-           
+
           </div>
         );
       })}
@@ -74,8 +77,314 @@ const ComplexData = () => {
 
 
 
-const LinksData = ()=>{
+const LinksData = () => {
+  const data = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+          "lat": "-43.9509",
+          "lng": "-34.4618"
+        }
+      },
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+      }
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "address": {
+        "street": "Douglas Extension",
+        "suite": "Suite 847",
+        "city": "McKenziehaven",
+        "zipcode": "59590-4157",
+        "geo": {
+          "lat": "-68.6102",
+          "lng": "-47.0653"
+        }
+      },
+      "phone": "1-463-123-4447",
+      "website": "ramiro.info",
+      "company": {
+        "name": "Romaguera-Jacobson",
+        "catchPhrase": "Face to face bifurcated interface",
+        "bs": "e-enable strategic applications"
+      }
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "address": {
+        "street": "Hoeger Mall",
+        "suite": "Apt. 692",
+        "city": "South Elvis",
+        "zipcode": "53919-4257",
+        "geo": {
+          "lat": "29.4572",
+          "lng": "-164.2990"
+        }
+      },
+      "phone": "493-170-9623 x156",
+      "website": "kale.biz",
+      "company": {
+        "name": "Robel-Corkery",
+        "catchPhrase": "Multi-tiered zero tolerance productivity",
+        "bs": "transition cutting-edge web services"
+      }
+    },
+    {
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+      "address": {
+        "street": "Skiles Walks",
+        "suite": "Suite 351",
+        "city": "Roscoeview",
+        "zipcode": "33263",
+        "geo": {
+          "lat": "-31.8129",
+          "lng": "62.5342"
+        }
+      },
+      "phone": "(254)954-1289",
+      "website": "demarco.info",
+      "company": {
+        "name": "Keebler LLC",
+        "catchPhrase": "User-centric fault-tolerant solution",
+        "bs": "revolutionize end-to-end systems"
+      }
+    },
+    {
+      "id": 6,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "email": "Karley_Dach@jasper.info",
+      "address": {
+        "street": "Norberto Crossing",
+        "suite": "Apt. 950",
+        "city": "South Christy",
+        "zipcode": "23505-1337",
+        "geo": {
+          "lat": "-71.4197",
+          "lng": "71.7478"
+        }
+      },
+      "phone": "1-477-935-8478 x6430",
+      "website": "ola.org",
+      "company": {
+        "name": "Considine-Lockman",
+        "catchPhrase": "Synchronised bottom-line interface",
+        "bs": "e-enable innovative applications"
+      }
+    },
+    {
+      "id": 7,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "email": "Telly.Hoeger@billy.biz",
+      "address": {
+        "street": "Rex Trail",
+        "suite": "Suite 280",
+        "city": "Howemouth",
+        "zipcode": "58804-1099",
+        "geo": {
+          "lat": "24.8918",
+          "lng": "21.8984"
+        }
+      },
+      "phone": "210.067.6132",
+      "website": "elvis.io",
+      "company": {
+        "name": "Johns Group",
+        "catchPhrase": "Configurable multimedia task-force",
+        "bs": "generate enterprise e-tailers"
+      }
+    },
+    {
+      "id": 8,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "email": "Sherwood@rosamond.me",
+      "address": {
+        "street": "Ellsworth Summit",
+        "suite": "Suite 729",
+        "city": "Aliyaview",
+        "zipcode": "45169",
+        "geo": {
+          "lat": "-14.3990",
+          "lng": "-120.7677"
+        }
+      },
+      "phone": "586.493.6943 x140",
+      "website": "jacynthe.com",
+      "company": {
+        "name": "Abernathy Group",
+        "catchPhrase": "Implemented secondary concept",
+        "bs": "e-enable extensible e-tailers"
+      }
+    },
+    {
+      "id": 9,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "email": "Chaim_McDermott@dana.io",
+      "address": {
+        "street": "Dayna Park",
+        "suite": "Suite 449",
+        "city": "Bartholomebury",
+        "zipcode": "76495-3109",
+        "geo": {
+          "lat": "24.6463",
+          "lng": "-168.8889"
+        }
+      },
+      "phone": "(775)976-6794 x41206",
+      "website": "conrad.com",
+      "company": {
+        "name": "Yost and Sons",
+        "catchPhrase": "Switchable contextually-based project",
+        "bs": "aggregate real-time technologies"
+      }
+    },
+    {
+      "id": 10,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "email": "Rey.Padberg@karina.biz",
+      "address": {
+        "street": "Kattie Turnpike",
+        "suite": "Suite 198",
+        "city": "Lebsackbury",
+        "zipcode": "31428-2261",
+        "geo": {
+          "lat": "-38.2386",
+          "lng": "57.2232"
+        }
+      },
+      "phone": "024-648-3804",
+      "website": "ambrose.net",
+      "company": {
+        "name": "Hoeger LLC",
+        "catchPhrase": "Centralized empowering task-force",
+        "bs": "target end-to-end models"
+      }
+    }
+  ]
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+{data.map((v, i) => {
+  return (
+    <div key={i} >
+      <div className="col">
+        <div className="card h-100">
+          <div className="card-body">
+            <h5 className="card-title id">{v.id}</h5>
+            <h5 className="card-title">{v.name}</h5>
+            <p className="card-text">{v.email}</p>
+            <p className="card-text">{v.address.city}</p>
+            <p className="card-text">{v.phone}</p>
+            <a href="#">{v.website}</a>
+            {console.log(v.address.city)}
+          </div>
+        </div>
+      </div>
+    </div>
 
+
+  // {/* <p>{v.id}</p>
+  //           <p>{v.name}</p>
+  //           <p>{v.username}</p>
+  //           <p>{v.email}</p>
+  //           <p>{v.address.city}</p>
+  //           <p>{v.phone}</p>
+  //           <p>{v.website}</p> */}
+
+
+
+
+
+
+  )
+})}
+</div>
+  )
 }
 
-export { NameComponent, ObjName, ArrData, ObjListData, ComplexData,LinksData };
+export { NameComponent, ObjName, ArrData, ObjListData, ComplexData, LinksData };
+  {/* <p>{v.id}</p>
+            <p>{v.name}</p>
+            <p>{v.username}</p>
+            <p>{v.email}</p>
+            <p>{v.address.city}</p>
+            <p>{v.phone}</p>
+            <p>{v.website}</p> */}
+{/* <div className="row row-cols-1 row-cols-md-3 g-4">
+{data.map((v, i) => {
+  return (
+    <div key={i} >
+      <div className="col">
+        <div className="card h-100">
+
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural lead-in
+              to additional content. This content is a little bit longer.
+            </p>
+            {console.log(v.address.city)}
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+  )
+})}
+</div> */}
